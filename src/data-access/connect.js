f29cddf49167fe373dcfb26f98481aa9cb2ed5ef
Urchmaney <kingsobino@gmail.com>
2020-07-02 22:47:44 +0000

    Add connector to MongoDB

---
+++ src/data-access/connect.js
@@ -0,0 +1,19 @@
+const { connect, connection } = require('mongoose');
+const wholesalerService = require('./services/wholesalerService');
+
+const mongoConnect = async (URI) => {
+  await connect(URI,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    err => {
+      if (err) {
+        console.log('Error connecting to Mongo DB', err);
+        process.exit(1);
+      }
+    });
+  return {
+    wholesalerService,
+    closeConnect: () => connection.close(),
+  };
+};
+
+module.exports = mongoConnect;
