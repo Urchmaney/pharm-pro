65598f7de0fc0bea2f5bb056d0599efce26e9b2e
Urchmaney <kingsobino@gmail.com>
2020-07-13 22:26:36 +0000

    Add swagger documentation for wholesaler model

--- src/data_access/schemas/wholesaler_schema.js
+++ src/data_access/schemas/wholesaler_schema.js
@@ -1,10 +1,42 @@
 const { Schema, model } = require('mongoose');
 
+
+/**
+ * @swagger
+ *   definitions:
+ *    Wholesaler:
+ *      type: object
+ *      required:
+ *        - fullName
+ *        - phoneNumber
+ *      properties:
+ *        fullName:
+ *          type: string
+ *        companyName:
+ *          type: string
+ *        licenseNumber:
+ *          type: string
+ *        address:
+ *          type: string
+ *        lga:
+ *          type: string
+ *        state:
+ *          type: string
+ *        body:
+ *          type: string
+ *        phoneNumber:
+ *          type: string
+ *        registrationNumber:
+ *          type: string
+ *
+ */
 const wholesalerSchema = new Schema({
   fullName: { type: String, required: true },
   companyName: String,
   licenseNumber: String,
   address: String,
+  lga: String,
+  state: String,
   body: String,
   phoneNumber: { type: String, required: true },
   registrationNumber: { type: String, required: true },
