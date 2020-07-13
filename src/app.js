361eef2e286eaf78240ba4518f4a16cc70910da8
Urchmaney <kingsobino@gmail.com>
2020-07-13 22:26:12 +0000

    Put port in variable and serve swagger in home page.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('./swagger/swaggerOption');
 require('dotenv').config();
 
 const mongoDB = require('./data_access/connect');
@@ -23,6 +25,8 @@ const startApplication = async () => {
   app.use(express.urlencoded({ extended: false }));
 
   app.use('/api/wholesalers', wholesalerRouter);
+
+  app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 
   return app;
 };
