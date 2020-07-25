828bcfb2c076c7c1f13d3bab7a9f98e964a0ed28
Urchmaney <kingsobino@gmail.com>
2020-07-25 08:58:40 +0000

    Add product controller

---
+++ src/controllers/productController.js
@@ -0,0 +1,45 @@
+const productController = (productService) => {
+  const create = {
+    roles: [],
+    action: async (product) => {
+      const { status, result } = await productService.createProduct(product);
+      if (status) return { statusCode: 201, result };
+      return { statusCode: 400, result };
+    },
+  };
+
+  const index = {
+    roles: [],
+    action: async () => {
+      const products = await productService.getProducts();
+      return { statusCode: 200, result: products };
+    },
+  };
+
+  const show = {
+    roles: [],
+    action: async (id) => {
+      const product = await productService.getProduct(id);
+      if (product) return { statusCode: 200, result: product };
+      return { statusCode: 400, result: 'invalid id.' };
+    },
+  };
+
+  const update = {
+    roles: [],
+    action: async (id, newProduct) => {
+      const updatedProduct = await productService.updatedProduct(id, newProduct);
+      if (updatedProduct) return { statusCode: 200, result: updatedProduct };
+      return { statusCode: 400, result: 'invalid id.' };
+    },
+  };
+
+  return {
+    index,
+    create,
+    show,
+    update,
+  };
+};
+
+module.exports = productController;
