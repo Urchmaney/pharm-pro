dc46368fff3e8ac3874cf006b1d0c0d03615a856
Urchmaney <kingsobino@gmail.com>
2020-09-02 22:52:40 +0000

    Add list feature - feature to close a list - feature to get a single list

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -2,13 +2,36 @@ const listController = (invoiceService) => {
   const index = {
     roles: [],
     action: async (retailerId, active) => {
-      const list = await invoiceService.getList(retailerId, active);
-      return { statusCode: 200, result: list };
+      const lists = await invoiceService.getLists(retailerId, active);
+      return { statusCode: 200, result: lists };
+    },
+  };
+
+  const show = {
+    roles: [],
+    action: async (retailerId, listId) => {
+      const list = await invoiceService.getList(retailerId, listId);
+      if (list) return { statusCode: 200, result: list };
+
+      return { statusCode: 400, result: 'Invalid list Id.' };
+    },
+  };
+
+  const closeList = {
+    roles: [],
+    action: async (retailerId, listId) => {
+      const list = await invoiceService.getList(retailerId, listId);
+      if (!list) return { statusCode: 400, result: 'Invalid list Id.' };
+
+      await invoiceService.closeList(listId, retailerId);
+      return { statusCode: 200, result: 'succefully closed list.' };
     },
   };
 
   return {
     index,
+    show,
+    closeList,
   };
 };
 
