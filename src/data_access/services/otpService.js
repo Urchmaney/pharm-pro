feb128003201c95ee517dad2ad86aa17dcb48a27
Urchmaney <kingsobino@gmail.com>
2020-09-06 18:54:31 +0000

    resolve issue with otp endpoint.

--- src/data_access/services/otpService.js
+++ src/data_access/services/otpService.js
@@ -38,7 +38,10 @@ const validateOTP = async (phoneNumber, userType, otp, date) => {
   return false;
 };
 
-const getOTPS = async (phoneNumber) => (await OtpModel.find({ phoneNumber }).sort('-expiration'))[0].otp;
+const getOTPS = async (phoneNumber) => {
+  const otps = await OtpModel.find({ phoneNumber }).sort('-expiration');
+  return otps.length ? otps[0].otp : 'none';
+};
 
 module.exports = {
   getOTP,
