e5251b6a50f0c05a346cc98d23f6bce3b9379430
Urchmaney <kingsobino@gmail.com>
2020-09-06 18:43:02 +0000

    replace \n in firebase private key in environment.

--- src/notification/notifier.js
+++ src/notification/notifier.js
@@ -8,7 +8,7 @@ const client = twilio(accountSid, authToken);
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert({
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     projectId: process.env.FIREBASE_PROJECT_ID,
   }),
